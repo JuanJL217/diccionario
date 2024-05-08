a9a5bcd275a21e3237eebd631cdba99de18cd202
JuanJL217 <juandocumentos217@gmail.com>
2024-05-08 06:29:36 -0300

    Aguante la recursividad

--- abb.go
+++ abb.go
@@ -7,18 +7,13 @@ type nodoABB[K comparable, V any] struct {
 	dato      V
 }
 
+type funcCmp[K comparable] func(K, K) int
+
 type abb[K comparable, V any] struct {
 	raiz     *nodoABB[K, V]
 	cantidad int
 	cmp      funcCmp[K] // porque devuelve menor, igual o mayor a 0
 }
-
-type nodoAuxiliar[K comparable, V any] struct { //La idea de este nodo es saber de donde vengo, o sea, tener un nodo actual, donde estoy parado, mas que odo para borra
-	actual   *nodoABB[K, V] // Padre
-	anterior *nodoABB[K, V] // el Padre del actual, de donde vengo
-}
-
-type funcCmp[K comparable] func(K, K) int
 
 func CrearABB[K comparable, V any](funcion_cmp func(K, K) int) DiccinarioOrdenado[K, V] {
 	return &abb[K, V]{nil, 0, funcion_cmp}
@@ -29,11 +24,32 @@ func crearNodo[K comparable, V any](clave K, dato V) *nodoABB[K, V] {
 }
 
 func (arbol *abb[K, V]) Guardar(clave K, dato V) {
-	// AYUDAAAAA
-	if arbol.raiz == nil {
-		arbol.raiz.clave, arbol.raiz.dato = clave, dato
+	nuevoNodo := crearNodo(clave, dato)
+	actual, padre := arbol.buscarNodo(clave, arbol.raiz, nil) // pongo nil, porque antes de la raiz, no hay nada
+	if actual == nil && padre == nil {
+		arbol.raiz = nuevoNodo
+		arbol.cantidad++
+	} else if actual != nil {
+		actual.dato = dato
 	} else {
-		arbol.Guardar(clave, dato)
+		if arbol.cmp(clave, padre.clave) < 0 {
+			padre.izquierdo = nuevoNodo
+		} else {
+			padre.derecho = nuevoNodo
+		}
+		arbol.cantidad++
 	}
-	arbol.cantidad++
+}
+
+func (arbol *abb[K, V]) buscarNodo(clave K, nodoActual *nodoABB[K, V], padre *nodoABB[K, V]) (*nodoABB[K, V], *nodoABB[K, V]) {
+	if nodoActual == nil {
+		return nil, padre
+	}
+	if arbol.cmp(clave, nodoActual.clave) == 0 {
+		return nodoActual, padre
+	} else if arbol.cmp(clave, nodoActual.clave) < 0 {
+		return arbol.buscarNodo(clave, nodoActual.izquierdo, nodoActual)
+	} else {
+		return arbol.buscarNodo(clave, nodoActual.derecho, nodoActual)
+	}
 }
